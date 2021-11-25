93dd728dabaad88be9b67612c735bad45b88d5c5
Tomoyuki Sakurai <y@trombik.org>
2021-11-25 12:08:01 +0700

    imp: support API
    API requires TLS. icinga2 has CLI tools to manage CA and certificates,
    but their implementation is rather buggy. use cfssl for certificate
    management.

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -9,6 +9,7 @@ config_dir = case os[:family]
              else
                "/etc/icinga2/icinga2.conf"
              end
+data_dir = "/var/lib/icinga2"
 ports = []
 # log_dir = "/var/log/icinga2"
 # TODO features = %w[checker ido-pgsql mainlog notification]
@@ -35,6 +36,9 @@ config_files = %w[
 db_user = "icinga_ido"
 db_name = "icinga_ido"
 db_password = "password"
+api_user = "root"
+api_password = "0660d951f4a29e8b"
+api_endpoint = "https://localhost:5665/v1"
 
 describe package(package) do
   it { should be_installed }
@@ -73,4 +77,10 @@ describe command "env PGPASSWORD=#{db_password} psql --host 127.0.0.1 --user #{d
   its(:exit_status) { should eq 0 }
   its(:stderr) { should eq "" }
   its(:stdout) { should match(/zone_id\s+\|\s+instance_id\s+\|\s+zone_object_id\s+\|\s+parent_zone_object_id\s+\|\s+config_type\s+\|\s+is_global\s+\|\s+config_hash/) }
+end
+
+describe command "curl -v --user #{api_user}:#{api_password} --cacert #{data_dir}/certs/ca.crt #{api_endpoint}" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should match(/SSL certificate verify ok/) }
+  its(:stdout) { should match(%r{You are authenticated as <b>root<\/b>}) }
 end
